5e8053b044a981e3d8636960bdd51df0474366cc
manishdashsharma <mdashsharma95@gmail.com>
2025-09-05 13:34:26 +0530

    Add initial backend setup with Express, MongoDB, and logging
    - Created essential project files including package.json, nodemon.json, and .gitignore.
    - Implemented Express server with basic health check routes.
    - Configured MongoDB connection and integrated rate limiting.
    - Added logging functionality using Winston with MongoDB transport.
    - Established environment configuration using dotenv.
    - Included error handling middleware and response utilities.
    - Set up initial logging structure with development and production configurations.
    - Added necessary dependencies for server functionality and development tools.

---
+++ src/router/apiRouter.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import healthController from '../controller/Health/health.controller.js'
+
+const router = Router()
+
+// Health routes
+router.route('/self').get(healthController.self)
+router.route('/health').get(healthController.health)
+
+export default router
---
+++ src/util/errorObject.js
@@ -0,0 +1,27 @@
+import responseMessage from '../constant/responseMessage.js';
+import config from '../config/config.js';
+import { EApplicationEnvironment } from '../constant/application.js';
+// import logger from './logger';
+
+export default (err, req, errorStatusCode = 500) => {
+    const errorObj = {
+        success: false,
+        statusCode: errorStatusCode,
+        request: {
+            ip: req.ip || null,
+            method: req.method,
+            url: req.originalUrl,
+        },
+        message: err instanceof Error ? err.message || responseMessage.ERROR.SOMETHING_WENT_WRONG : responseMessage.ERROR.SOMETHING_WENT_WRONG,
+        data: null,
+        trace: err instanceof Error ? { error: err.stack } : null,
+    };
+
+    // Production Env check
+    if (config.env === EApplicationEnvironment.PRODUCTION) {
+        delete errorObj.request.ip;
+        delete errorObj.trace;
+    }
+
+    return errorObj;
+};
